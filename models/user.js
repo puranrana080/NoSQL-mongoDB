155f469f601fe160256a34cc56148f944f671139
Puran Singh Rana <puransinghrana080@gmail.com>
2024-12-10 23:26:48 +0530

    display cart items, delete cart item

--- models/user.js
+++ models/user.js
@@ -26,16 +26,19 @@ class User {
       return cp.productId.toString() === product._id.toString();
     });
     let newQuantity = 1;
-    const updatedCartItems=[...this.cart.items];
+    const updatedCartItems = [...this.cart.items];
 
     if (cartProductIndex >= 0) {
       newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-      updatedCartItems[cartProductIndex].quantity=newQuantity
-    }else{
-      updatedCartItems.push({ productId: new mongodb.ObjectId(product._id), quantity: newQuantity })
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new mongodb.ObjectId(product._id),
+        quantity: newQuantity,
+      });
     }
     const updatedCart = {
-      items: updatedCartItems
+      items: updatedCartItems,
     };
     const db = getDb();
     return db
@@ -43,6 +46,40 @@ class User {
       .updateOne(
         { _id: new mongodb.ObjectId(this._id) },
         { $set: { cart: updatedCart } }
+      );
+  }
+
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((i) => {
+      return i.productId;
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity,
+          };
+        });
+      });
+  }
+
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter((item) => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } }
       );
   }
 
