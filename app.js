4c538d7e8f54b1fd05e76a312d37c295b880b9e0
Puran Singh Rana <puransinghrana080@gmail.com>
2024-12-10 22:40:12 +0530

    add to cart ,multiple products

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("67584ffa2e677836249f925f")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name,user.email,user.cart,user._id);
       next();
     })
     .catch((err) => console.log(err));
