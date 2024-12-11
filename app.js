880a96c37870a254968db51e09fda341bf7a73d1
Puran Singh Rana <puransinghrana080@gmail.com>
2024-12-11 20:38:05 +0530

    intro of mongoose , product model

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
+
 const User = require("./models/user");
+const mongoose =require('mongoose')
 
 const app = express();
 
@@ -18,20 +19,27 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("67584ffa2e677836249f925f")
-    .then((user) => {
-      req.user = new User(user.name,user.email,user.cart,user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("67584ffa2e677836249f925f")
+//     .then((user) => {
+//       req.user = new User(user.name,user.email,user.cart,user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+
+mongoose.connect('mongodb+srv://puranrana:Puran080@cluster0.066ki.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0'
+  )
+  .then(()=>{
+    app.listen(3000)
+    console.log("connected")
+  })
+  .catch(err=>{
+    console.log(err)
+  })
