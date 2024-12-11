bb30a3ef7ae63516e3da7bdb065b2d6b84d7bad3
Puran Singh Rana <puransinghrana080@gmail.com>
2024-12-11 23:12:30 +0530

    deleteCart

--- models/user.js
+++ models/user.js
@@ -15,7 +15,7 @@ const userSchema = new Schema({
       {
         productId: {
           type: Schema.Types.ObjectId,
-          re: "Product",
+          ref: "Product",
           required: true,
         },
         quantity: {
@@ -27,7 +27,7 @@ const userSchema = new Schema({
   },
 });
 
-userSchema.methods.addToCart = function(product) {
+userSchema.methods.addToCart = function (product) {
   const cartProductIndex = this.cart.items.findIndex((cp) => {
     return cp.productId.toString() === product._id.toString();
   });
@@ -50,6 +50,13 @@ userSchema.methods.addToCart = function(product) {
   return this.save();
 };
 
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
 
 module.exports = mongoose.model("User", userSchema);
 
